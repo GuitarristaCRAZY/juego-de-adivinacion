c969d82076da717df1a4da273c6ffeb4b355f347
CRAZY <88456072+GuitarristaCRAZY@users.noreply.github.com>
2026-07-04 16:41:47 -0400

    Update game.py

--- game.py
+++ game.py
@@ -18,31 +18,38 @@ def intro():
 
 
 def play():
-    num_sec = random.randint(1, 100)
-    max_intentos = intro()
-    intentos = 0
+    while True:
+        num_sec = random.randint(1, 100)
+        max_intentos = intro()
+        intentos = 0
 
-    print("adivina el numero secreto entre el 1 y 100: ")
-    print(f"\n Tienes {max_intentos} intentos para adivinar")
+        print("adivina el numero secreto entre el 1 y 100: ")
+        print(f"\n Tienes {max_intentos} intentos para adivinar")
 
-    while intentos < max_intentos:
-        try:
-            guess = int(input(f"Intento {intentos + 1}/{max_intentos} - Introduce tu número: "))
-        except ValueError:
-            print("Por favor, ingresa un número válido.")
-            continue
-            
-        intentos += 1
+        while intentos < max_intentos:
+            try:
+                guess = int(input(f"Intento {intentos + 1}/{max_intentos} - Introduce tu número: "))
+            except ValueError:
+                print("Por favor, ingresa un número válido.")
+                continue
+                
+            intentos += 1
 
-        if guess < num_sec:
-            print("Demasiado bajo.")
-        elif guess > num_sec:
-            print("Demasiado alto.")
-        else:
-            print(f"¡Felicidades! Has adivinado en {intentos} intentos.")
-            return # Termina la función al ganar
+            if guess < num_sec:
+                print("Demasiado bajo.")
+            elif guess > num_sec:
+                print("Demasiado alto.")
+            else:
+                print(f"¡Felicidades! Has adivinado en {intentos} intentos.")
+                return # Termina la función al ganar
 
-    print(f"\n¡Se te han acabado los intentos! El número era: {num_sec}")
+        print(f"\n¡Se te han acabado los intentos! El número era: {num_sec}")
+
+        Reintento = input("Quieres volver a intentar? (S/N)\n").upper()
+        if Reintento != "S":
+            print("¡Gracias por jugar! Adiós.")
+            break
+
 
 # Iniciar el juego
 if __name__ == "__main__":
